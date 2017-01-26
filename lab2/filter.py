30225fb2df8f554c6e0965e3133bf01df2394d7d
sbonada <santiago.bonada@uoit.net>
2017-01-25 20:02:31 -0500

    corrected task 1

--- lab2/filter.py
+++ lab2/filter.py
@@ -7,11 +7,19 @@ import math
 from matplotlib import pyplot as plt
 
 def gaussian2d(mu,cov,n):
-    kernel = np.zeros([n,n])
-    first = 1.0/(2.0*math.pi*cov*cov)
+    center = n/2
+    sum = 0
+    row = np.linspace(-center,center,n)
+    xi,yi = np.meshgrid(row,row)
+    kernel = yi
+
+    first = 1.0/(math.sqrt(2.0*math.pi)*cov*cov) # the non-varying coefficient
     for k in range(n):
         for l in range(n):
-            kernel[k][l] = first * GaussExp(k,l,mu,cov)
+            next = first * GaussExp(xi[k][l],yi[k][l],mu,cov)
+            sum = sum + next
+            kernel[k][l] = next
+    kernel = kernel / sum
     return kernel;
 
 def GaussExp(k,l,mu,cov):
@@ -20,7 +28,7 @@ def GaussExp(k,l,mu,cov):
 def filter(img,task):
     # Complete this method according to the tasks listed in the lab handout.
     if task == 1:
-        kernel = gaussian2d(0,1,5)
+        kernel = gaussian2d(0,5,5)
     elif task == 2:
         kernel = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
     elif task == 3:
@@ -74,7 +82,9 @@ if __name__ == '__main__':
     parser.add_argument('imgfile', help='Image file')
     args = parser.parse_args()
 
+    task = 1
+
     if args.use_plotlib:
-        process_img2(args.imgfile,2)
+        process_img2(args.imgfile,task)
     else:
-        process_img1(args.imgfile,2)
+        process_img1(args.imgfile,task)
